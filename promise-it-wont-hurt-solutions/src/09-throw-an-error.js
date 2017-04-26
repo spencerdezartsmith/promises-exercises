71fe1937c54e9b066309644a862fe180463f9dab
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-04-26 14:13:24 -0700

    Completed throw-an-error. NOTE: There is a bug in this challenge therefore test didn not pass fully

---
+++ promise-it-wont-hurt-solutions/src/09-throw-an-error.js
@@ -0,0 +1,12 @@
+const parsePromised = (json) => {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(JSON.parse(json))
+    } catch(e){
+      reject(e)
+    }
+  })
+}
+
+parsePromised(process.argv[0])
+  .then(null, console.log)
